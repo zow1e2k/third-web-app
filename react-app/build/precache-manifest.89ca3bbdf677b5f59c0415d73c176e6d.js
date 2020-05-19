3bed268aae94c404290bb4580e3babf6a3a423aa
Artyom <kadomodify@gmail.com>
2020-05-19 20:22:45 +0300

    prod. version

---
+++ react-app/build/precache-manifest.89ca3bbdf677b5f59c0415d73c176e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853167daf8f5709aecdc4285feecd03c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7d1520806f0b60a33e1",
+    "url": "/static/css/main.972d335b.chunk.css"
+  },
+  {
+    "revision": "6ebd52de62379cd83ba1",
+    "url": "/static/js/2.d35d38b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d35d38b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d1520806f0b60a33e1",
+    "url": "/static/js/main.79337980.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
